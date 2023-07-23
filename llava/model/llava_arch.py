5e89b7ed725f6bd026aa0245d5a0f948e85d613a
Qingyun <79644233+Li-Qingyun@users.noreply.github.com>
2023-07-23 20:22:40 +0800

    Use `self.get_model().get_vision_tower()` instead of `self.get_model().vision_tower` in `llava_arch.py` (#299)
    * fix llava_arch.py
    
    * Update llava_arch.py

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -79,7 +79,7 @@ class LlavaMetaForCausalLM(ABC):
         return self.get_model().get_vision_tower()
 
     def encode_images(self, images):
-        image_features = self.get_model().vision_tower(images)
+        image_features = self.get_model().get_vision_tower()(images)
         image_features = self.get_model().mm_projector(image_features)
         return image_features
 
