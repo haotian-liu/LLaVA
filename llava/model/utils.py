1384ec0a9331d79ad31008a01284b00202317d83
Vinayak Mali <66154908+malivinayak@users.noreply.github.com>
2023-10-13 23:46:10 +0530

    making the sentence grammatically correct

--- llava/model/utils.py
+++ llava/model/utils.py
@@ -5,7 +5,7 @@ def auto_upgrade(config):
     cfg = AutoConfig.from_pretrained(config)
     if 'llava' in config and 'llava' not in cfg.model_type:
         assert cfg.model_type == 'llama'
-        print("You are using newer LLaVA code base, while the checkpoint of v0 is from older code base.")
+        print("You are using a newer LLaVA code base, while the checkpoint of v0 is from an older code base.")
         print("You must upgrade the checkpoint to the new code base (this can be done automatically).")
         confirm = input("Please confirm that you want to upgrade the checkpoint. [Y/N]")
         if confirm.lower() in ["y", "yes"]:
