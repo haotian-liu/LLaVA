c9f19e97acca62b89d118fd7387c8f4003f5fcc4
teowu <realtimothyhwu@gmail.com>
2023-10-15 19:21:58 +0800

    evaluation scripts for Q-Bench

---
+++ scripts/v1_5/eval/qbench_zh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+python -m llava.eval.model_vqa_qbench \
+    --model-path liuhaotian/llava-v1.5-13b \
+    --image-folder ./playground/data/qbench/images_llvisionqa/ \
+    --question-file ./playground/data/eval/qbench/llvisionqa_$1.json \
+    --answers-file ./playground/data/eval/qbench/llvisionqa_zh_$1_answers.jsonl \
+    --conv-mode llava_v1
+    --lang zh
