8fa7f58b5947f5a0cb71b5c32b023a9ed893aa97
l-salewski <71447327+l-salewski@users.noreply.github.com>
2023-11-24 14:55:43 +0100

    🩹 make ``load_pretrained_model`` accept kwargs
    - this allows it to e.g. take a ``cache_dir`` argument

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -23,8 +23,8 @@ from llava.model import *
 from llava.constants import DEFAULT_IMAGE_PATCH_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 
 
-def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, load_4bit=False, device_map="auto", device="cuda"):
-    kwargs = {"device_map": device_map}
+def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, load_4bit=False, device_map="auto", device="cuda", **kwargs):
+    kwargs = {"device_map": device_map, **kwargs}
 
     if device != "cuda":
         kwargs['device_map'] = {"": device}
