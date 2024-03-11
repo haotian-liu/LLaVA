e7618ee118968f36116771d83f3dcc55a752e851
Mac.Ari <d.aritra@outlook.com>
2024-03-10 23:13:11 -0700

    Adding Pre-train and Fine tune to branch

---
+++ scripts/Mistral/finetune-lora.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+deepspeed llava/train/train-flash_attention_2.py \
+  --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+  --deepspeed ./scripts/zero3.json \
+  --model_name_or_path mistralai/Mistral-7B-v0.1 \
+  --version plain \
+  --data_path ./playground/data/LLaVA-Finetune/llava_train_QCM-LEA.json \
+  --image_folder ./playground/data/LLaVA-Finetune/images \
+  --vision_tower openai/clip-vit-large-patch14-336 \
+  --pretrain_mm_mlp_adapter ./checkpoints/llava-mistral-7b-pretrain/mm_projector.bin \
+  --mm_projector_type mlp2x_gelu \
+  --mm_vision_select_layer -2 \
+  --mm_use_im_start_end False \
+  --mm_use_im_patch_token False \
+  --image_aspect_ratio pad \
+  --group_by_modality_length True \
+  --bf16 True \
+  --output_dir ./checkpoints/llava-mistral-7b \
+  --num_train_epochs 10 \
+  --per_device_train_batch_size 8 \
+  --per_device_eval_batch_size 4 \
+  --gradient_accumulation_steps 1 \
+  --evaluation_strategy "no" \
+  --save_strategy "epoch" \
+  --save_steps 1 \
+  --save_total_limit 11 \
+  --learning_rate 2e-5 \
+  --weight_decay 0. \
+  --warmup_ratio 0.03 \
+  --lr_scheduler_type "cosine" \
+  --logging_steps 1 \
+  --tf32 True \
+  --model_max_length 2048 \
+  --gradient_checkpointing True \
+  --dataloader_num_workers 4 \
+  --lazy_preprocess True \
+  --report_to wandb
---
+++ scripts/Mistral/pretrain.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+deepspeed ./llava/train/train.py \
+  --deepspeed ./scripts/zero2.json \
+  --model_name_or_path mistralai/Mistral-7B-v0.1 \
+  --version plain \
+  --data_path ./playground/data/LLaVA-Pretrain/LLaVA-CC3M-Pretrain-595K.json \
+  --image_folder ./playground/data/LLaVA-Pretrain/images \
+  --vision_tower openai/clip-vit-large-patch14-336 \
+  --mm_projector_type mlp2x_gelu \
+  --tune_mm_mlp_adapter True \
+  --mm_vision_select_layer -2 \
+  --mm_use_im_start_end False \
+  --mm_use_im_patch_token False \
+  --bf16 True \
+  --output_dir ./checkpoints/llava-mistral-7b-pretrain \
+  --cache_dir ./cache \
+  --num_train_epochs 1 \
+  --per_device_train_batch_size 32 \
+  --per_device_eval_batch_size 4 \
+  --gradient_accumulation_steps 1 \
+  --evaluation_strategy "no" \
+  --save_strategy "steps" \
+  --save_steps 24000 \
+  --save_total_limit 1 \
+  --learning_rate 1e-3 \
+  --weight_decay 0. \
+  --warmup_ratio 0.03 \
+  --lr_scheduler_type "cosine" \
+  --logging_steps 1 \
+  --tf32 True \
+  --model_max_length 2048 \
+  --gradient_checkpointing True \
+  --dataloader_num_workers 4 \
+  --lazy_preprocess True \
+  --report_to wandb
