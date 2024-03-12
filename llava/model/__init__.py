5c134a57223b2874fce4703271424dd0bdec9055
Aritra Das <ardas@ucsd.edu>
2024-03-12 23:19:32 +0000

    Few updates

--- llava/model/__init__.py
+++ llava/model/__init__.py
@@ -1,6 +1,6 @@
 try:
-    from .language_model.llava_llama import LlavaLlamaForCausalLM, LlavaConfig
-    from .language_model.llava_mpt import LlavaMptForCausalLM, LlavaMptConfig
-    from .language_model.llava_mistral import LlavaMistralForCausalLM, LlavaMistralConfig
+    from llava.model.language_model.llava_llama import LlavaLlamaForCausalLM, LlavaConfig
+    from llava.model.language_model.llava_mpt import LlavaMptForCausalLM, LlavaMptConfig
+    from llava.model.language_model.llava_mistral import LlavaMistralForCausalLM, LlavaMistralConfig
 except:
     pass
--- scripts/Mistral/eval/sqa_mistral_7b_pretrain.sh
+++ scripts/Mistral/eval/sqa_mistral_7b_pretrain.sh
@@ -2,12 +2,12 @@
 
 python -m llava.eval.model_vqa_science \
     --model-path ./checkpoints/llava-mistral-7b-pretrain \
+    --model-base mistralai/Mistral-7B-v0.1 \
     --question-file ./playground/data/eval/scienceqa/llava_test_CQM-A.json \
     --image-folder ./playground/data/eval/scienceqa/images/test \
     --answers-file ./playground/data/eval/scienceqa/answers/llava-mistral-7b-pretrain.jsonl \
-    --model-base mistralai/Mistral-7B-v0.1 \
     --single-pred-prompt \
-    --conv_mode mistral_instruct \
+    --conv-mode mistral_instruct \
     --temperature 0
 
 python llava/eval/eval_science_qa.py \
