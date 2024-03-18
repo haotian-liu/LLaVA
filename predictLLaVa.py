1a09f51ce7402515e54e16e9e180ad61cf7a4b6a
Jonas <68696759+MrMrJay@users.noreply.github.com>
2024-03-18 16:27:39 +0100

    reduced gpu usage through 4bit loading

--- predictLLaVa.py
+++ predictLLaVa.py
@@ -24,7 +24,8 @@ model_path = "liuhaotian/llava-v1.6-mistral-7b"
 tokenizer, model, image_processor, context_len = load_pretrained_model(
     model_path=model_path,
     model_base=None,
-    model_name=get_model_name_from_path(model_path)
+    model_name=get_model_name_from_path(model_path),
+    load_4bit=True
 )
 
 instruction = ""
