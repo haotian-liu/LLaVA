2ee12a5a868984e68435a3f8e2376f946dc2487b
TongmengXie <114957703+TongmengXie@users.noreply.github.com>
2024-03-23 18:13:45 +0000

    Update finetune_lora.sh

--- scripts/finetune_lora.sh
+++ scripts/finetune_lora.sh
@@ -14,22 +14,21 @@
 # MODEL_VERSION="llama-2-7b-chat"
 ################## LLaMA-2 ##################
 
-!deepspeed LLaVA/llava/train/train_mem.py \
-    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
-    --deepspeed LLaVA/scripts/zero3.json \
-    --model_name_or_path liuhaotian/llava-v1.5-13b \
-    --version v1 \
-    --data_path LLaVA/playground/data/llava_v1_5_mix665k.json \
-    --image_folder LLaVA/playground/data \
-    --vision_tower openai/clip-vit-large-patch14-336 \
-    --mm_projector_type mlp2x_gelu \
+deepspeed llava/train/train_mem.py \
+    --deepspeed ./scripts/zero2.json \
+    --lora_enable True \
+    --bits 4 \
+    --model_name_or_path ./checkpoints/$MODEL_VERSION \
+    --version $PROMPT_VERSION \
+    --data_path ./playground/data/llava_instruct_80k.json \
+    --image_folder /path/to/coco/train2017 \
+    --vision_tower openai/clip-vit-large-patch14 \
+    --pretrain_mm_mlp_adapter ./checkpoints/llava-$MODEL_VERSION-pretrain/mm_projector.bin \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --image_aspect_ratio pad \
-    --group_by_modality_length True \
     --bf16 True \
-    --output_dir LLaVA/checkpoints/llava-v1.5-13b-task-lora \
+    --output_dir ./checkpoints/llava-$MODEL_VERSION-finetune_lora \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
@@ -38,7 +37,7 @@
     --save_strategy "steps" \
     --save_steps 50000 \
     --save_total_limit 1 \
-    --learning_rate 2e-4 \
+    --learning_rate 2e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
@@ -46,6 +45,6 @@
     --tf32 True \
     --model_max_length 2048 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
     --lazy_preprocess True \
+    --dataloader_num_workers 4 \
     --report_to wandb
