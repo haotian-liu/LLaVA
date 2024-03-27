1bf4898f006c43564faf09a153c8731985281f27
Ariel N. Lee <103224818+arielnlee@users.noreply.github.com>
2024-03-27 16:21:43 -0400

    fine-tune llava-1.6 mistral 7b and 34b

---
+++ scripts/v1_6/finetune_lora_llava_34b.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+deepspeed llava/train/train_mem.py \
+    --lora_enable True --lora_r 16 --lora_alpha 32 --mm_projector_lr 2e-5 \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path liuhaotian/llava-v1.6-34b \
+    --version chatml_direct_ft \
+    --data_path combined_data.json \
+    --image_folder random_images \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --mm_patch_merge_type spatial_unpad \
+    --image_aspect_ratio anyres \
+    --group_by_modality_length False \
+    --bf16 False \
+    --fp16 True \
+    --output_dir ./llava-lora-34b \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 450 \
+    --save_total_limit 5 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.05 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 4096 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb \
