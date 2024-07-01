92af8c717642e73379d4ffa751898d682a7fcdf9
inkcherry <mingzhi.liu@intel.com>
2024-07-01 03:26:26 +0000

    automatically added to python module search path

--- llava/train/train_mem.py
+++ llava/train/train_mem.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+# Ensure the project root directory is 
+# automatically added to Python module search path
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+if root_dir not in sys.path:
+    sys.path.insert(0, root_dir)
+    
 from llava.train.train import train
 
 if __name__ == "__main__":
